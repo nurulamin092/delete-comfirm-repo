864908b014c1fd1642643c8385ade9f35774fa86
nurulamin092 <mnurulamin092@gmial.com>
2022-02-16 12:48:48 +0600

    delete confirm

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ document.getElementById('delete-btn').
     addEventListener('click', function () {
         document.getElementById('secret-info').style.display = 'none'
     });
+
+document.getElementById('delete-confirm').
+    addEventListener('keyup', function (event) {
+        const deleteBtm = document.getElementById('delete-btn')
+        if (event.target.value == 'delete') {
+            deleteBtm.removeAttribute('disabled')
+        }
+        else {
+            deleteBtm.setAttribute('disabled', true);
+        }
+    });
